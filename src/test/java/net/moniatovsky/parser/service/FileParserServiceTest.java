705616b421e7d4f06c63fe9aaeb15f1f84094c7e
AlexMoniatovsky <amoniatovskiy@cloudlinux.com>
2020-04-06 11:12:05 +0300

    Apply google code style to test

--- src/test/java/net/moniatovsky/parser/service/FileParserServiceTest.java
+++ src/test/java/net/moniatovsky/parser/service/FileParserServiceTest.java
@@ -1,26 +1,26 @@
 package net.moniatovsky.parser.service;
 
+import java.io.File;
 import net.moniatovsky.parser.model.DirectoryReport;
 import net.moniatovsky.parser.model.FileReport;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
-
 public class FileParserServiceTest {
 
-    private ParseFileService fileParserService = new ParseFileService();
+  private ParseFileService fileParserService = new ParseFileService();
 
-    @Test
-    public void testDirectory() {
-        File directory = new File("src/test/resources/testDir");
-        DirectoryReport expectedRootReport = DirectoryReport.of("testDir", 0);
-        expectedRootReport.addFile(FileReport.of("Test1.java", 3));
-        DirectoryReport expectedSubReport = DirectoryReport.of("subDir", 1);
-        expectedSubReport.addFile(FileReport.of("Test3.java", 5));
-        expectedSubReport.addFile(FileReport.of("Test2.java", 6));
-        expectedRootReport.addDirectory(expectedSubReport);
-        DirectoryReport resultReport = fileParserService.buildDirectoryReport(directory, 0);
-        Assert.assertEquals("Result report should be equal to expected report", expectedRootReport, resultReport);
-    }
+  @Test
+  public void testDirectory() {
+    File directory = new File("src/test/resources/testDir");
+    DirectoryReport expectedRootReport = DirectoryReport.of("testDir", 0);
+    expectedRootReport.addFile(FileReport.of("Test1.java", 3));
+    DirectoryReport expectedSubReport = DirectoryReport.of("subDir", 1);
+    expectedSubReport.addFile(FileReport.of("Test3.java", 5));
+    expectedSubReport.addFile(FileReport.of("Test2.java", 6));
+    expectedRootReport.addDirectory(expectedSubReport);
+    DirectoryReport resultReport = fileParserService.buildDirectoryReport(directory, 0);
+    Assert.assertEquals("Result report should be equal to expected report", expectedRootReport,
+        resultReport);
+  }
 }
